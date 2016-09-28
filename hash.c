944752e3b2ce1aac2697505b14b9a04e997c09f2
Lucky <lucaslois95@gmail.com>
2016-09-28 02:30:42 -0300

    Prueba Lucky

--- hash.c
+++ hash.c
@@ -25,7 +25,8 @@ int hash_function(char *key_string){
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 
     hash_t * hash = malloc(sizeof(hash_t));
-    if(hash==NULL)return NULL;
+    if(hash==NULL)
+        return NULL;
     hash->length = 0;
     hash->busy = LARGO;
     hash_node_t * hash_array = malloc(sizeof(hash_node_t)*hash->length);
